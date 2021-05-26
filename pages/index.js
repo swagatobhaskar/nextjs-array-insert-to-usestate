0c96e2711b1bd9cf51c9ee5c9b34c2013fd2ea01
Swagato Bhaskar <50166675+swagatobhaskar@users.noreply.github.com>
2021-05-27 01:31:40 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+export default function IndexPage() {
+  const [tag, setTag] = useState("");
+  const [tags, setTags] = useState([]);
+  const [diag, setDiag] = useState("");
+
+  const addTag = ({ target }) => {
+    setTag(target.value);
+  };
+
+  const addThisTag = (tag) => {
+    setTags((tags) => [...tags, tag]);
+    setTag("");
+    console.log("Added: ", tags);
+  };
+
+  const sendTags = () => {
+    setDiag("Sent tags: ", tags);
+  };
+
+  return (
+    <div>
+      <h4>Hello there!</h4>
+      <div style={{ border: "2px solid green" }}>
+        <div>
+          <input
+            type="text"
+            name="tags"
+            value={tag}
+            placeholder="Search tags"
+            onChange={addTag}
+            //onChange={(e) => setTag(e.target.value)}
+          />
+          <button
+            type="submit"
+            style={{
+              border: "none",
+              backgroundColor: "gold",
+              margin: "0px 2px"
+            }}
+            onClick={() => addThisTag(tag)}
+          >
+            Add
+          </button>
+        </div>
+        <br />
+        Tags:{" "}
+        {tags.map((tag) => (
+          <li key={tag} style={{ color: "green", fontWeight: "bolder" }}>
+            {tag}
+          </li>
+        ))}
+      </div>
+      <button onClick={sendTags}>Submit</button>
+      {diag.length > 0 ? (
+        <h3 style={{ color: "purple" }}>Sent tags: {tags}</h3>
+      ) : null}
+    </div>
+  );
+}
