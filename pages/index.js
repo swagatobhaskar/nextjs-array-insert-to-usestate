a163f0858b77230375fdff180cffe1d290ec77ba
Swagato Bhaskar <50166675+swagatobhaskar@users.noreply.github.com>
2021-05-27 01:34:05 +0530

    removed one console.log()

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,6 @@ export default function IndexPage() {
   const addThisTag = (tag) => {
     setTags((tags) => [...tags, tag]);
     setTag("");
-    console.log("Added: ", tags);
   };
 
   const sendTags = () => {
